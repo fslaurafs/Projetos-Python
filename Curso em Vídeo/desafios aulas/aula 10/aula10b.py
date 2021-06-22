4341afb682ed8911d258134821e27ff4fbc73d0c
Laura F. Sorato <laurafernandessorato@gmail.com>
2021-06-22 19:16:43 -0300

    Delete Curso em Vídeo/desafios aulas directory

--- Curso em Vídeo/desafios aulas/aula 10/aula10b.py
+++
@@ -1,7 +0,0 @@
-# CONDIÇÕES
-nome = str(input("Qual o seu nome? "))
-if nome == 'Laura':
-    print("Que nome lindo você tem!")
-else:
-    print("Seu nome é tão... sei lá, normal!")
-print("Olá, {}!".format(nome))
--- Curso em Vídeo/desafios aulas/aula 6/aula6.py
+++
@@ -1,5 +0,0 @@
-# DATA DE NASCIMENTO
-dia = int(input('Informe o dia do seu nascimento: '))
-mes = input('Informe o mês de se nascimento: ')
-ano = int(input('Informe o ano de seu nascimento: '))
-print('Você nasceu no dia {} de {} de {}!'.format(dia, mes, ano))

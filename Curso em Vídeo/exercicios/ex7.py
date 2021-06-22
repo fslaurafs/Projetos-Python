c2a999c249805835c0983ec482d90930939b710b
Laura F. Sorato <laurafernandessorato@gmail.com>
2021-06-22 17:59:10 -0300

    Exercícios 6 a 10

---
+++ Curso em Vídeo/exercicios/ex7.py
@@ -0,0 +1,6 @@
+# NOTAS E MÉDIA
+a = input('Informe o aluno: ')
+n1 = float(input('Informe a nota 1: '))
+n2 = float(input('Informe a nota 2: '))
+m = (n1 + n2) / 2
+print('A média de {} é {:.2f}!'.format(a, m))
